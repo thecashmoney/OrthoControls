a1c6c5c05089bafa51b7d500359d9126b362ba7c
thecashmoney <elliottjang1@gmail.com>
2025-05-03 12:50:06 -0700

    add more files

---
+++ src/main/java/com/thecashmoney/orthocontrols/Orthocontrols.java
@@ -0,0 +1,10 @@
+package com.thecashmoney.orthocontrols;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Orthocontrols implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
