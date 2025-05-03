ac00ee0e631f974d01050ce947c64319b86e64d5
thecashmoney <elliottjang1@gmail.com>
2025-05-03 16:35:58 -0700

    final file addition

---
+++ src/client/java/com/thecashmoney/orthocontrols/client/OrthocontrolsClient.java
@@ -0,0 +1,10 @@
+package com.thecashmoney.orthocontrols.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OrthocontrolsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
