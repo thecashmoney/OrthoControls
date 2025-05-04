1748a25f97d4dc073d97e4b137807de009958169
thecashmoney <elliottjang1@gmail.com>
2025-05-03 18:12:41 -0700

    started on mouse separation from game

--- src/client/java/com/thecashmoney/orthocontrols/client/OrthocontrolsClient.java
+++ src/client/java/com/thecashmoney/orthocontrols/client/OrthocontrolsClient.java
@@ -2,9 +2,18 @@ package com.thecashmoney.orthocontrols.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import net.minecraft.client.MinecraftClient;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
 public class OrthocontrolsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+//        MinecraftClient client = MinecraftClient.getInstance();
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.mouse != null && client.player != null) {
+                client.mouse.unlockCursor();  // Safe here
+            }
+        });
     }
 }
